7657c9a8028dc4480767d62605a188c76a3bdf3a
Ashley <ashleydavies493@gmail.com>
2024-05-30 16:53:29 +0100

    Update header.php

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cappee</title>
+    <title>Cappee.</title>
     <link rel="stylesheet" href="<?php echo $style; ?>">
 </head>
 <body>
